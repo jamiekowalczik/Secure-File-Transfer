4afd841dd5f2e53d68e7216ed512917e15e2ec7a
Jamie Kowalczik <jamie.kowalczik@gmail.com>
2019-01-14 22:07:37 -0500

    refactored to use one function for both get and put

--- Secure-File-Transfer.ps1
+++ Secure-File-Transfer.ps1
@@ -14,17 +14,17 @@ Try{
 
 <#
    .SYNOPSIS
-      This will securely retrieve files using the sftp or scp protocol.
+      This will securely get or retrieve files using the sftp or scp protocol.
 
    .DESCRIPTION
       This function will securely retrieve files using the sftp or scp protocol.  
       Optionally you can specify a server's fingerprint to be verified prior to sending files.
 
    .EXAMPLE
-      Get-Files -Username "usera" -Password "passworda" -Source "C:\data\*" -Destination "/upload/data/"
+      Secure-File-Transfer -Hostname "fileserver" -Username "usera" -Password "passworda" -Direction "put" -Source "C:\data\*" -Destination "/upload/data/"
 
    .EXAMPLE
-      Get-Files -Username "usera" -Password "passworda" -Method "SCP" -Fingerprint "rsa-2048 xxxxxxxxxxxx" -Source "C:\data\*" -Destination "/upload/data/"
+      Secure-File-Transfer -Hostname "fileserver" -Username "usera" -Password "passworda" -Direction "put" -Method "SCP" -Fingerprint "rsa-2048 xxxxxxxxxxxx" -Source "C:\data\*" -Destination "/upload/data/"
 
    .NOTES
       For this function to work you must reference the WinSCP Library.  The function will send files using sftp or scp, defaulting to sftp.  
@@ -33,23 +33,25 @@ Try{
    .LINK
       http://winscp.net/eng/docs/library_install
 #>
-Function Get-Files{
+Function Secure-File-Transfer{
    [CmdletBinding()]
    Param(
       # The remote server's IP address or FQDN
-      [String]$Hostname,
+      [Parameter(Mandatory=$true)][String]$Hostname,
       # The username to login to the remote server with
-      [String]$Username,
+      [Parameter(Mandatory=$true)][String]$Username,
       # The password to login to the remote server
-      [String]$Password,
+      [Parameter(Mandatory=$true)][String]$Password,
+      # The direction for the file transfer - GET or PUT
+      [Parameter(Mandatory=$true)][String]$Direction = "",
       # The protocol to use for file transfer - SFTP or SCP
       [String]$Method = "SFTP",
       # The remote server's fingerprint.  This paramter is optional
       [String]$Fingerprint,
       # The source file or directory for the transfer
-      [String]$Source,
+      [Parameter(Mandatory=$true)][String]$Source,
       # The destination file or directory for the transfer
-      [String]$Destination
+      [Parameter(Mandatory=$true)][String]$Destination
    )
   
    If($DebugScript){
@@ -96,103 +98,23 @@ Function Get-Files{
 
    $transferOptions = New-Object WinSCP.TransferOptions
    $transferOptions.TransferMode = [WinSCP.TransferMode]::Binary
- 
-   $transferResult = $session.GetFiles($Source, $Destination, $False, $transferOptions)
- 
-   $transferResult.Check()
 
-   $session.Dispose()
-
-   Return $transferResult
-}
-<#
-   .SYNOPSIS
-      This will securely transfer files using the sftp or scp protocol.
-
-   .DESCRIPTION
-      Put-Files will securely transfer files using the sftp or scp protocol.  
-      Optionally you can specify a server's fingerprint to be verified prior to sending files.
-
-   .EXAMPLE
-      Put-Files -Username "usera" -Password "passworda" -Source "C:\data\*" -Destination "/upload/data/"
-
-   .EXAMPLE
-      Put-Files -Username "usera" -Password "passworda" -Method "SCP" -Fingerprint "rsa-2048 xxxxxxxxxxxx" -Source "C:\data\*" -Destination "/upload/data/"
-
-   .NOTES
-      For this function to work you must reference the WinSCP Library.  The function will send files using sftp or scp, defaulting to sftp.  
-      Supplying a server's fingerprint is optional.
-
-   .LINK
-      http://winscp.net/eng/docs/library_install
-#>
-Function Put-Files{
-   [CmdletBinding()]
-   Param(
-      # The remote server's IP address or FQDN
-      [String]$Hostname,
-      # The username to login to the remote server with
-      [String]$Username,
-      # The password to login to the remote server
-      [String]$Password,
-      # The protocol to use for file transfer - SFTP or SCP
-      [String]$Method = "SFTP",
-      # The remote server's fingerprint.  This paramter is optional
-      [String]$Fingerprint,
-      # The source file or directory for the transfer
-      [String]$Source,
-      # The destination file or directory for the transfer
-      [String]$Destination
-   )
-
-   If($DebugScript){
-      $CommandName = $PSCmdlet.MyInvocation.InvocationName;
-      # Get the list of parameters for the command
-      $ParameterList = (Get-Command -Name $CommandName).Parameters;
-      $now = Get-Date; Write-Host "`n$($MyInvocation.MyCommand.Name):: started $now"
-      # Grab each parameter value, using Get-Variable
-	  $FunctionVariables = ""
-      foreach ($Parameter in $ParameterList) {
-         $FunctionVariables = Get-Variable -Name $Parameter.Values.Name -ErrorAction SilentlyContinue | Out-String
-      }
-	 Write-Host "$FunctionVariables"
-   }
-
-
-   # Configure the sessions options.  
-   $sessionOptions = New-Object WinSCP.SessionOptions
-
-   # If a transfer method is not specified then default to SFTP.
-   switch ($Method) {
-      "sftp" {
-         $sessionOptions.Protocol = [WinSCP.Protocol]::Sftp
+   # Determine whether to put or get
+   switch ($Direction) {
+      "put" {
+         $transferResult = $session.PutFiles($Source, $Destination, $False, $transferOptions)
          break
       }
-      "scp" {
-         $sessionOptions.Protocol = [WinSCP.Protocol]::Scp
-         Write-Host "using scp"
+      "get" {
+         $transferResult = $session.GetFiles($Source, $Destination, $False, $transferOptions)
          break
       }
       default {
-         $sessionOptions.Protocol = [WinSCP.Protocol]::Sftp
+         Write-Host "Invalid direction specified.  Please use PUT or GET"
+         Exit 1
          break
       }
    }
-   
-   # If a fingerprint is not specified then turn of hostkey checking
-   If($Fingerprint -eq ""){ $sessionOptions.GiveUpSecurityAndAcceptAnySshHostKey = $true } Else { $sessionOptions.SshHostKeyFingerprint = $Fingerprint }
-
-   $sessionOptions.HostName = $Hostname
-   $sessionOptions.UserName = $Username
-   $sessionOptions.Password = $Password
-   
-   $session = New-Object WinSCP.Session
-   $session.Open($sessionOptions)
-
-   $transferOptions = New-Object WinSCP.TransferOptions
-   $transferOptions.TransferMode = [WinSCP.TransferMode]::Binary
- 
-   $transferResult = $session.PutFiles($Source, $Destination, $False, $transferOptions)
  
    $transferResult.Check()
 
