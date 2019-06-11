c75355b23663a8034e444ced95322c03c8d38027
jamiekowalczik <jamie.kowalczik@gmail.com>
2019-06-10 20:36:05 -0400

    Update test-transfer.ps1

--- test-transfer.ps1
+++ test-transfer.ps1
@@ -1,3 +1,9 @@
 $TransferResult = Invoke-SecureFileTransfer -Hostname "fileserver" -Username "root" -Password "rootpassword" -Direction get -Source "/data/*" -Destination "C:\data\"
 
 $TransferResult | Out-String
+
+#################
+
+$TransferResult = Invoke-SecureFileTransfer -Hostname "fileserver" -Username "root" -SshPrivateKeyPath "C:\privatekey.ppk" -Direction "put" -Method "SCP" -Source "C:\data\*" -Destination "/upload/data/"
+
+$TransferResult | Out-String
